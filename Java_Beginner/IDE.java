8a46e825c43d7c58c30568016d1670492c1b83c0
H@r! <s.hariharancs@gmail.com>
2017-10-28 15:42:33 +0530

    Create IDE.java

---
+++ Java_Beginner/IDE.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class IDE
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+      String name="Eclipse Luna";
+      double version = 4.4;
+      System.out.println("Eclipse name: "+name+"\n Version: "+version);
+	}
+}
