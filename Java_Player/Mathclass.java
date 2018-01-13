d02cd23ede38b2c65adb17755563413a1f6358fa
H@r! <s.hariharancs@gmail.com>
2018-01-13 12:52:23 +0530

    Create Mathclass.java

---
+++ Java_Player/Mathclass.java
@@ -0,0 +1,16 @@
+
+package examples;
+
+
+public class Mathclass {
+public static void main(String args[]){
+    double a=2.23452;
+    System.out.println("e = " + Math.E);
+    System.out.println("pi = " + Math.PI);
+    System.out.println("Ceil = " + Math.ceil(a));
+    System.out.println("Floor = " + Math.floor(a));
+    System.out.println("Rounded = " + Math.round(a));
+    System.out.println("Squared = " + Math.pow(a, 2));
+    System.out.println("Square root = " + Math.sqrt(a));
+}    
+}
