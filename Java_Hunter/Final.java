2b943f803462802c76df38dc6a222fda4f6b1366
H@r! <s.hariharancs@gmail.com>
2018-01-13 12:56:42 +0530

    Create Final.java

---
+++ Java_Hunter/Final.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+final class Final{
+final String course_name;
+Final(){ 
+course_name="JAVA"; 
+}
+final void print()
+ {
+System.out.println(course_name); 
+}
+public static void main(String args[]){ 
+Final a =  new Final(); 
+ a.print();
+} 
+} 
