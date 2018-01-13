333c0eeb993bf57d6f3cb7604214f6d7c5d62cbe
H@r! <s.hariharancs@gmail.com>
2018-01-13 12:56:16 +0530

    Create strs.java

---
+++ Java_Hunter/strs.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class str {
+    public static void main(String args[]){
+        String s1=new String("Guvi");
+        String s2=new String("Guvi");
+        System.out.println("using equals: "+ s1.equals(s2));
+        System.out.println("Using == "+( s1==s2));
+    }
+}
