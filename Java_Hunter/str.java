dab9dc3623df38ae774c42a80e10b1dcd11fd9cc
H@r! <s.hariharancs@gmail.com>
2018-01-13 12:55:15 +0530

    Create str.java

---
+++ Java_Hunter/str.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+package examples;
+
+
+public class str {
+    public static String str(char a[]){
+         String s=new String(a);
+        
+        return s;
+        
+    }
+    public static void main(String args[]){
+        char a[]={'H','a','r','i'};
+       
+        System.out.println(str(a));
+    }
+}
