76b7ede2c79e17dfe2b8433bfa960d5afd724e23
H@r! <s.hariharancs@gmail.com>
2018-01-13 12:57:49 +0530

    Create Stringinstanceof.java

---
+++ Java_Hunter/Stringinstanceof.java
@@ -0,0 +1,9 @@
+public class Stringinstanceof {
+ public static void main(String[] a) {
+String s = "Hari from accet";
+System.out.println("Whether String object is instance of lang.string:"+s instanceof java.lang.String);
+if (s instanceof java.lang.String) {
+System.out.println("Inside if statement after checking our string object");
+}
+  }
+}
